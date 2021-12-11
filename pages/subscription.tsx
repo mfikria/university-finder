f5a4d91fd1acd6979b7910fe15f11e7d7c142052
mfikria <mfikria@gmail.com>
2021-12-11 18:08:22 +0700

    feat: subscription page

--- pages/subscription.tsx
+++ pages/subscription.tsx
@@ -1,14 +1,51 @@
 import { NextPage } from 'next'
 import SiteHeader from '../components/SiteHeader'
-import { Layout } from 'antd'
-import styles from './index.module.scss'
+import { Layout, Form, Input, Button, Typography, Card } from 'antd'
+import styles from './subscription.module.scss'
+const { Title } = Typography
+
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+  },
+}
+
+const onSubmit = () => {
+  // TODO: submit to backend
+}
 
 const Subscription: NextPage = () => {
   return (
     <Layout>
       <SiteHeader />
       <Layout.Content className={styles.siteLayout}>
-        <div className={styles.siteLayoutBackground}></div>
+        <Card bordered={false} className={styles.subscriptionCard}>
+          <Title level={3}>Subscribe and access all our features!</Title>
+          <Form
+            name="nest-messages"
+            onFinish={onSubmit}
+            validateMessages={validateMessages}
+          >
+            <Form.Item
+              name={'email'}
+              label="Email"
+              rules={[
+                {
+                  required: true,
+                  type: 'email',
+                },
+              ]}
+            >
+              <Input placeholder="Input email to subscribe" />
+            </Form.Item>
+            <Form.Item className={styles.subscriptionCardFooter}>
+              <Button type="primary" htmlType="submit">
+                Subscribe
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
       </Layout.Content>
     </Layout>
   )
