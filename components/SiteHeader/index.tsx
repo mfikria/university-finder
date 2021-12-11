f77b456e5cb99cc22d7f4ba2a5dd0e43a6843107
mfikria <mfikria@gmail.com>
2021-12-11 22:25:17 +0700

    feat: add account menu

--- components/SiteHeader/index.tsx
+++ components/SiteHeader/index.tsx
@@ -1,7 +1,10 @@
 import styles from './style.module.scss'
-import { Menu, Layout } from 'antd'
+import { Menu, Layout, Dropdown, Row, Col, Avatar } from 'antd'
 import { useRouter } from 'next/router'
 import { MenuInfo } from 'rc-menu/lib/interface'
+import { useSession } from 'next-auth/react'
+import { AiOutlineUser } from 'react-icons/ai'
+import Link from 'next/link'
 
 type NavigationType = {
   title: string
@@ -9,6 +12,7 @@ type NavigationType = {
 }
 
 const SiteHeader = () => {
+  const { data: session } = useSession()
   const router = useRouter()
   const navigations: NavigationType[] = [
     {
@@ -25,18 +29,42 @@ const SiteHeader = () => {
     router.push(item.key)
   }
 
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        <Link href="/api/auth/signout">Logout</Link>
+      </Menu.Item>
+    </Menu>
+  )
+
   return (
     <Layout.Header className={styles.siteHeader}>
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        onClick={onClickMenu}
-        selectedKeys={[router.pathname]}
-      >
-        {navigations.map((nav) => (
-          <Menu.Item key={nav.url}>{nav.title}</Menu.Item>
-        ))}
-      </Menu>
+      <Row>
+        <Col flex="auto">
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            onClick={onClickMenu}
+            selectedKeys={[router.pathname]}
+          >
+            {navigations.map((nav) => (
+              <Menu.Item key={nav.url}>{nav.title}</Menu.Item>
+            ))}
+          </Menu>
+        </Col>
+        <Col className={styles.accountMenu}>
+          {session ? (
+            <Dropdown overlay={menu}>
+              <span>
+                <Avatar icon={<AiOutlineUser />} />
+                {session?.user?.email}
+              </span>
+            </Dropdown>
+          ) : (
+            <Link href="/api/auth/signin">LOGIN</Link>
+          )}
+        </Col>
+      </Row>
     </Layout.Header>
   )
 }
