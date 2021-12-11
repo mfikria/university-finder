21b918e8c94a09f4c313e4c93a41cfb2653e9fb5
mfikria <mfikria@gmail.com>
2021-12-11 14:17:05 +0700

    feat: home layout

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, {
+  Html,
+  DocumentContext,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
+
+import React from 'react'
+
+React.useLayoutEffect = React.useEffect
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
