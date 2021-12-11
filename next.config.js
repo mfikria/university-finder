da484df8ffb5e56c7e7381e85359611ffd39ff9a
mfikria <mfikria@gmail.com>
2021-12-11 08:56:02 +0700

    feat: install jest and antd ui framework

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+  modifyVars: { '@primary-color': '#04f' }, // optional
+  // lessVarsFilePath: './src/styles/variables.less', // optional
+  lessVarsFilePathAppendToEndOfContent: false, // optional
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {
+    // ...
+    mode: "local",
+    localIdentName: "[path][name]__[local]--[hash:base64:5]", // invalid! for Unify getLocalIdent (Next.js / CRA), Cannot set it, but you can rewritten getLocalIdentFn
+    exportLocalsConvention: "camelCase",
+    exportOnlyLocals: false,
+    // ...
+    // getLocalIdent: (context, localIdentName, localName, options) => {
+    //   return "whatever_random_class_name";
+    // },
+  },
+
+  // Other Config Here...
+
+  webpack(config) {
+    return config;
+  },
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { Button } from 'antd'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -16,6 +17,8 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Button type="primary">Primary Button</Button>
 
         <p className={styles.description}>
           Get started by editing{' '}
